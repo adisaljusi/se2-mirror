38306b865fa77eafeba1d2120b3d38c1b4a92854
adisaljusi <adis.aljusi@hotmail.com>
2025-11-03 13:47:54 +0100

    Create FiveBucksVoucherTest

---
+++ freelance4u/src/test/java/ch/zhaw/freelance4u/model/voucher/PercentageVoucherTest.java
@@ -0,0 +1,46 @@
+package ch.zhaw.freelance4u.model.voucher;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import ch.zhaw.freelance4u.model.Job;
+import ch.zhaw.freelance4u.model.JobType;
+
+public class PercentageVoucherTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = { 1, 2, 5, 20, 49, 50 })
+    public void testSingleJobWithVariousPercentages(int percentage) {
+        PercentageVoucher voucher = new PercentageVoucher(percentage);
+        List<Job> jobs = new ArrayList<>();
+
+        Job job = new Job("Test Job", "Test Description", JobType.IMPLEMENT, 50.0, "");
+        jobs.add(job);
+
+        double discount = voucher.getDiscount(jobs);
+        double expected = 50.0 * (percentage / 100.0);
+
+        assertEquals(expected, discount);
+    }
+
+    @Test
+    public void testTwoJobsWith42PercentDiscount() {
+        PercentageVoucher voucher = new PercentageVoucher(42);
+        List<Job> jobs = new ArrayList<>();
+
+        Job job1 = new Job("Test Job 1", "Test Description 1", JobType.IMPLEMENT, 42.0, "");
+        Job job2 = new Job("Test Job 2", "Test Description 2", JobType.IMPLEMENT, 77.0, "");
+        jobs.add(job1);
+        jobs.add(job2);
+
+        double discount = voucher.getDiscount(jobs);
+
+        assertEquals(49.98, discount);
+    }
+}
