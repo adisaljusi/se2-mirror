5b0aea7875270dfd625318f761f735969e4a4039
adisaljusi <40966163+adisaljusi@users.noreply.github.com>
2025-11-03 13:42:15 +0100

    Implement and test FiveBucksVoucher (#25)
    * Implement FiveBucksVoucher class
    
    * Create test cases for FiveBucksVoucher

---
+++ freelance4u/src/main/java/ch/zhaw/freelance4u/model/voucher/FiveBucksVoucher.java
@@ -0,0 +1,18 @@
+package ch.zhaw.freelance4u.model.voucher;
+
+import java.util.List;
+
+import ch.zhaw.freelance4u.model.Job;
+
+public class FiveBucksVoucher implements Voucher {
+
+    @Override
+    public double getDiscount(List<Job> jobs) {
+        double totalEarnings = jobs.stream()
+                .mapToDouble(Job::getEarnings)
+                .sum();
+
+        return totalEarnings >= 10.0 ? 5.0 : 0.0;
+    }
+
+}
