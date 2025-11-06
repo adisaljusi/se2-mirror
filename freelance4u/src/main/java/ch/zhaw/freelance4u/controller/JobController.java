86ffe86a69889ab432be1b046e31318241d5728f
adisaljusi <adis.aljusi@hotmail.com>
2025-11-06 22:58:37 +0100

    iadd tests for security

--- freelance4u/src/main/java/ch/zhaw/freelance4u/controller/JobController.java
+++ freelance4u/src/main/java/ch/zhaw/freelance4u/controller/JobController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -79,6 +80,15 @@ public class JobController {
         }
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteJobById(@PathVariable String id) {
+        if (!userService.userHasRole("admin")) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+        jobRepository.deleteById(id);
+        return ResponseEntity.status(HttpStatus.OK).body("DELETED");
     }
 
 }
